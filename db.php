ae00901c114789225444b0217de7475279180a5a
devanshslnk <29497979+devanshslnk@users.noreply.github.com>
2018-02-12 19:53:25 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php
+
+	$db_servername="localhost";
+	$db_username="root";
+	$db_password="";
+	$db_name="dj_hackathon";
+	$conn=mysqli_connect($db_servername,$db_username,$db_password,$db_name);
+
+
+
+?>
